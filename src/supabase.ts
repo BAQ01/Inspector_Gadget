db1fca32b933737e6ba704f92eec3650e9faf097
BAQ01 <baquispel@gmail.com>
2026-02-19 13:04:58 +0100

    env aanpassingen

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,8 @@
 // src/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-// Jouw project gegevens
-const SUPABASE_URL = 'https://blqpecudiddxwbidtdjk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscXBlY3VkaWRkeHdiaWR0ZGprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMzg2MDgsImV4cCI6MjA4NTYxNDYwOH0.91IuuSrl123zurAqueESzQChZZqvXKKamygb_Xdks_g';
+// Haal de beveiligde sleutels op uit het .env bestand
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
